a6b43ef0820bdb6ccb04ffb381d3a10810b0c0b9
olga.olegovna.603 <olga.olegovna.603@list.ru>
2025-11-15 20:25:15 +0300

    three commit

--- src/main/java/ru/productstar/mockito/Main.java
+++ src/main/java/ru/productstar/mockito/Main.java
@@ -16,7 +16,7 @@ public class Main {
             order = orderService.addProduct(order, "phone", 2, false);
             System.out.println("Общая сумма заказа после добавления телефона: " +
                     order.getTotal());
-            order = orderService.addProduct(order, "Laptop", 1, true);
+            order = orderService.addProduct(order, "laptop", 1, true);
             System.out.println("Общая сумма после добавления ноутбука с доставкой: " +
                     order.getTotal());
         } catch(Exception e) {
